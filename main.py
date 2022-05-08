2e7a9ebb9d7c599e88daf40dc02e8754ec4cb0c7
Vasiliy Shukshin <vasiliy.shukshin@yandex.ru>
2022-05-08 14:23:43 +0300

    получен словарь покупок

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 with open("cookbook.txt", "r", encoding="UTF-8") as cook_book_file:
-    # for line in cook_book_file:
-    #     # print(line)
-    #     print(line.strip().split(" | "))
-    #     # print(line.split("|"))
     cook_book = {}
     try:
         while True:
             name = cook_book_file.readline().strip()
             number_of_ingredients = int(cook_book_file.readline().strip())
-            # print(name)
-            # print(number_of_ingredients)
-            # cook_book = {name: cook_book_file.readline().strip().split(" | ") for a in range(number_of_ingredients)}
-            # ingredients = cook_book_file.readline().strip().split(" | ")
-            # for i in ingredients:
             i = 0
-            # ingredients_list = []
             super_ingrediet_listt = []
             while i < number_of_ingredients:
                 ingredients_list = cook_book_file.readline().strip().split(" | ")
                 ingredients_dict = {'ingredient_name': ingredients_list[0], 'quantity': ingredients_list[1], 'measure':ingredients_list[2]}
                 super_ingrediet_listt.append(ingredients_dict)
                 cook_book[name] = super_ingrediet_listt
-                # ingredients_list.append(cook_book_file.readline().strip().split(" | "))
                 i +=  1
-            # print(cook_book)
-            # print(ingredients_list)
             next(cook_book_file)
     except StopIteration:
-        # print(cook_book)
-        for key, value in cook_book.items():
-            print("{0}: {1}".format(key, value))
-
-            # next(cook_book_file)
-# d = {a: a ** 2 for a in range(7)}
+        None
+        # for key, value in cook_book.items():
+        #     print("{0}: {1}".format(key, value))
 
 
+def get_shop_list_by_dishes(dishes, person_count=int()):
+    shop_dict = {}
+    # для каждого блюда из  списка заказаннных блюд
+    for dish in dishes:
+        # если заказанное блюдо есть в кулинарной книге
+        if dish in cook_book:
+            # перебираем необходдимы продукты и добавляем в список покупок
+            for product in cook_book[dish]:
+                # если продукт уже есть в списке
+                if product['ingredient_name'] in shop_dict:
+                    shop_dict[product['ingredient_name']] = {'measure': product['measure'],
+                                                             'quantity': (int(product['quantity']) + int(product['quantity']))*person_count}
+                else:
+                    shop_dict[product['ingredient_name']] = {'measure': product['measure'],
+                                                             'quantity': int(product['quantity'])*person_count}
+            # print(shop_dict)
+        else:
+            print(f"{dish} - такого  блюда нет в cook_book")
+        # # print(shop_dict)
+
+    return print(shop_dict)
+
+get_shop_list_by_dishes(['Омлет', 'яичница'], 1)
